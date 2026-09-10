e67998245b13b66b6cdc4f63bbb39951a8988538
RadLeo OFC <radleo.official@gmail.com>
2026-09-10 23:02:25 +0300

    chore: add standalone MySQL development container

---
+++ docker/mysql/init/01-create-testing-database.sql
@@ -0,0 +1,5 @@
+-- UA: Окрема база для автоматизованих тестів.
+-- EN: A separate database for automated tests.
+CREATE DATABASE IF NOT EXISTS accommodation_booking_test
+    CHARACTER SET utf8mb4
+    COLLATE utf8mb4_unicode_ci;
